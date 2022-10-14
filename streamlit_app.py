7abeeef9ac607517109d097f60ae6ba43d9b88ef
Hrushik Perumalla <59475008+hrushikp@users.noreply.github.com>
2022-10-14 19:07:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,36 @@ try:
     streamlit.dataframe(back_from_fuction)
 except URLError as e:
   streamlit.error()
-      
-      
+
+
+stremalit.header("The fruit load list contains:")
+#snowflake-related fuctions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("SELECT * from fruit_load_list")
+      return my_cur.fetchall()
+   
+#add a button to load the fruits
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
+   
+
+#my_cur.execute("use warehouse pc_rivery_wh")
+#my_cur.execute("SELECT * from fruit_load_list")
+#my_data_row = my_cur.fetchall()
+#streamlit.header("the fruit load list contains:")
+
+
+#New Section to display fruityvice api response  to add
+streamlit.header("Fruityvice Fruit Advice!")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('thanks for adding ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
+#      
 streamlit.write('The user entered ', fruit_choice)
 
 
@@ -51,23 +79,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 #streamlit.text(fruityvice_response.json())#just wrrites teh data to teh screen
 
 # we are normalising the data from the json file
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display the table on the page
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 #dont run anything past here
-streamlit.stop()
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("use warehouse pc_rivery_wh")
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.header("the fruit load list contains:")
-streamlit.dataframe(my_data_row)
-
-#New Section to display fruityvice api response  to add
-streamlit.header("Fruityvice Fruit Advice!")
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.write('thanks for adding ', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#streamlit.stop()
